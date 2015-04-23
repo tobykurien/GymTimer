3232195668c7fc35176aa66948ab64a5fd56a03c
tobykurien <toby@tobykurien.com>
2015-04-23 09:30:10 +0200

    added flashing of LED during rest period

--- README.md
+++ README.md
@@ -4,11 +4,13 @@ Dead-simple arduino-based gym timer for StrongLifts 5x5 style gym routine
 # Usage
 
 - Power on device. 5 LED's light up to show that you have 5 sets left.
-- Complete your first set, then press button. One LED goes off and rest period begins.
-- Once buzzer sounds, press button to silence it.
-- Complete next set, then press button to enter rest period.
-- Once buzzer sounds, press button to silence it.
-- Keep going for the rest of the sets. If this was the last set, then process repeats from start.
+- Complete your first set, then press button. First LED blinks and rest period begins.
+- Once buzzer sounds, press button to silence it. The first LED goes off and shows you have 4 sets left.
+- Keep going for the rest of the sets, pressing the button each time you complete a set or finish your rest period.
+- If this was the last set, then the process repeats from start with 5 new sets.
+- Note that you can press the button *during* a rest period to cancel the timer for that rest period.
+
+You can change the number of sets (up to 10) and the rest period by editing the 2 variables (```REST_PERIOD_MS``` and ```MAX_SETS```) at the top of the source file.
 
 # Wiring
 
@@ -16,6 +18,7 @@ Dead-simple arduino-based gym timer for StrongLifts 5x5 style gym routine
 
 - Use an Arduino Uno
 - Connect 5 LED's via current-limiting resistors (e.g. 220ohm) to pins 3 to 7.
+- Optionally, connect more LED's (up to another 5) via current-limiting resistors (e.g. 220ohm) to pins 9 to 13.
 - Connect a piezo buzzer to pin 8 and down to ground.
 - Connect a push-button switch to pin 2 and down to ground.
 
--- gym_timer/gym_timer.ino
+++ gym_timer/gym_timer.ino
@@ -3,7 +3,7 @@
  * Toby Kurien
  */
 
-#define REST_PERIOD_MS 90000  // How long the rest period is
+#define REST_PERIOD_MS 90000  // How long the rest period is in milliseconds
 #define MAX_SETS       5      // How many sets (i.e. LEDs connected), up to 10
 
 // Pin assignments 
@@ -11,11 +11,18 @@ int button = 2;
 int buzzer = 8;
 int leds[] = {3, 4, 5, 6, 7, 9, 10, 11, 12, 13};
 
+// timer and set count
 int count = MAX_SETS;
 long debounceTime = 0;
 long timer = 0;
+long now = 0;
 
+// resting led blink stuff
 boolean resting = false;
+long restBlinkPeriod = 0;
+boolean restLedOn = true;
+
+// buzzer stuff
 boolean alarm = false;
 int buzzerOn = false;
 long alarmPeriod = 0;
@@ -45,22 +52,26 @@ void showCount() {
 
 // the loop routine runs over and over again forever:
 void loop() {
-  if (millis() - debounceTime > 500 && digitalRead(button) == LOW) {
+  now = millis();
+  
+  if (now - debounceTime > 500 && digitalRead(button) == LOW) {
     // button pressed
-    debounceTime = millis();
-    timer = millis();
+    debounceTime = now;
+    timer = now;
     
     if (alarm || resting) {
       // alarm is sounding or we're resting, turn off alarm/resting
       alarm = false;
       resting = false;
+      restLedOn = false;
       digitalWrite(buzzer, LOW);
 
       // if this was the last set, reset for next round      
       if (count == 0) {
         count = MAX_SETS;
-        showCount();
       }
+      
+      showCount();
     } else {
       // a set was completed
       resting = true;
@@ -74,18 +85,27 @@ void loop() {
         !alarm && 
         resting && 
         timer != 0 && 
-        millis() - timer > REST_PERIOD_MS) {
+        now - timer > REST_PERIOD_MS) {
     // resting period is over
     alarm = true;
     buzzerOn = true;
     digitalWrite(buzzer, HIGH);
   }
   
-  if (alarm && millis() - alarmPeriod > 60) {
+  if (alarm && now - alarmPeriod > 60) {
     // pulse the buzzer if alarm is on, for a more pleasing sound
-    alarmPeriod = millis();
+    alarmPeriod = now;
     digitalWrite(buzzer, buzzerOn ? LOW : HIGH);  
     buzzerOn = !buzzerOn;
   }
+  
+  if (resting && now - restBlinkPeriod > 500) {
+    // blink the LED of the set being rested for
+    restBlinkPeriod = now;
+    digitalWrite(leds[count], restLedOn ? LOW : HIGH);
+    restLedOn = !restLedOn;
+  }
+  
+  delay(10); // bit of battery saving
 }
 
